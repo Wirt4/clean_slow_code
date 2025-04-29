eeab0f9278fc2b41f7779608637a9fcb3e695fd6
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-04-28 20:46:29 -0700

    made module command-line argument

--- main.py
+++ main.py
@@ -5,7 +5,5 @@ from modules import shape_generator
 module_name = sys.argv[1]
 print("module name is", module_name)
 
-print("profiling demo version")
-cProfile.run("shape_generator.compute_total_area('demo')")
-print("profling clean version")
-cProfile.run("shape_generator.compute_total_area('clean')")
+print("profiling", module_name)
+cProfile.run("shape_generator.compute_total_area('{}')".format(module_name))
