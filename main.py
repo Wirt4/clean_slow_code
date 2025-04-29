85914903149d10fd43f82eb5e0300bdd80dc4d04
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-04-28 20:44:30 -0700

    added command line argument to main

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import cProfile
+import sys
 from modules import shape_generator
 
-print("profiling clean code version")
-cProfile.run("shape_generator.compute_total_area()")
-print("profling optimized version")
-cProfile.run("shape_generator.compute_total_area()")
+module_name = sys.argv[1]
+print("module name is", module_name)
+
+print("profiling demo version")
+cProfile.run("shape_generator.compute_total_area('demo')")
+print("profling clean version")
+cProfile.run("shape_generator.compute_total_area('clean')")
--- modules/shape_generator.py
+++ modules/shape_generator.py
@@ -1,18 +1,37 @@
 from modules import demo
+from modules import clean
 
 NUMBER_OF_SHAPES = 500000
 
 
-def compute_total_area():
-    shapes = create_shapes()
+def compute_total_area(module_name):
+    shapes = create_shapes(module_name)
     return sum(shape.area() for shape in shapes)
 
 
-def create_shapes():
+def create_clean_shapes():
     shapes = []
     for i in range(NUMBER_OF_SHAPES):
         if i % 2 == 0:
-            shapes.append(demo.Circle(i % 100 + 1))
+            shapes.append(clean.Circle(i % 100 + 1))
         else:
-            shapes.append(demo.Square(i % 100 + 1))
+            shapes.append(clean.Square(i % 100 + 1))
+        return shapes
+
+
+def create_optimized_shapes():
+    shapes = []
+    for i in range(NUMBER_OF_SHAPES):
+        if i % 2 == 0:
+            shapes.append(clean.Circle(i % 100 + 1))
+        else:
+            shapes.append(clean.Square(i % 100 + 1))
+    return shapes
+
+
+def create_shapes(module_name):
+    if module_name == "clean":
+        shapes = create_clean_shapes()
+    else:
+        shapes = create_optimized_shapes()
     return shapes
