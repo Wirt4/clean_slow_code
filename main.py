36bbe823b7bb7a472ebedf87b52972d7024a96c6
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-04-28 20:31:52 -0700

    added some logging

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import cProfile
 from modules import shape_generator
 
+print("profiling clean code version")
+cProfile.run("shape_generator.compute_total_area()")
+print("profling optimized version")
 cProfile.run("shape_generator.compute_total_area()")
