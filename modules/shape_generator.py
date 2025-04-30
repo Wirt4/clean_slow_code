b908d55e7831537a61c218953ebe1ba87efc8e57
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-04-29 19:02:01 -0700

    move list to class

--- modules/shape_generator.py
+++ modules/shape_generator.py
@@ -15,11 +15,20 @@ def side_from_index(index):
 
 
 def create_shapes(module_name):
-    shapes = []
-    w = wrapper.Wrapper(module_name)
+    shapes = Shapes(module_name)
+    shape_wrapper = wrapper.Wrapper(module_name)
+    j = 0
     for i in range(NUMBER_OF_SHAPES):
-        if i % 2 == 0:
-            shapes.append(w.Circle(side_from_index(i)))
-        else:
-            shapes.append(w.Square(side_from_index(i)))
-    return shapes
+        if j == 0:
+            shapes.shape_list.append(shape_wrapper.Circle(side_from_index(i)))
+        elif j == 1:
+            shapes.shape_list.append(shape_wrapper.Square(side_from_index(i)))
+        j += 1
+        j %= 2
+    return shapes.shape_list
+
+
+class Shapes:
+    def __init__(self, module_name):
+        self.wrapper = wrapper.Wrapper(module_name)
+        self.shape_list = []
