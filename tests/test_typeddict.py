f3612b57aa34faa8b950070b08391c0c43da48b3
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-05-08 18:01:53 -0700

    red

--- tests/test_typeddict.py
+++ tests/test_typeddict.py
@@ -11,8 +11,20 @@ class TestTypedDictMethods(unittest.TestCase):
 
     def test_get_area_of_circle(self):
         """
-        Confirm 'get_area_branching' returns the correct circle area.
+        Confirm the brancing 'get_area' returns the correct circle area.
         Use the same values from the oop test
         """
         circle: ShapeUnion = {"shape_type": ShapeType.CIRCLE, "width": 2, "height": 2}
         self.assertEqual(get_area(circle), pi)
+
+    def test_get_area_of_rectangle(self):
+        """
+        Confirm the branching "get_area" returns the correct rectangle area.
+        Use the same values from the oop test.
+        """
+        rectangle: ShapeUnion = {
+            "shape_type": ShapeType.RECTANGLE,
+            "width": 40,
+            "height": 11,
+        }
+        self.assertEqual(get_area(rectangle), 440)
